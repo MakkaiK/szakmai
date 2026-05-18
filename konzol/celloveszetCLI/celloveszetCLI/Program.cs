b5c6a010c1cd236dafbffccba81481b0ee570dc8
MakkaiK <90418493+MakkaiK@users.noreply.github.com>
2026-05-18 21:14:10 +0200

    Add files via upload

---
+++ konzol/celloveszetCLI/celloveszetCLI/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace celloveszetCLI
+{
+    internal class Program
+    {
+        public static int legnagyobb(int elsoloves, int masodikloves, int harmadikloves, int negyedikloves)
+        {
+            int legnagyobb = elsoloves;
+            if (legnagyobb < masodikloves)
+            {
+                legnagyobb = masodikloves;
+            }
+            if (legnagyobb < harmadikloves)
+            {
+                legnagyobb = harmadikloves;
+            }
+            if (legnagyobb < negyedikloves)
+            {
+                legnagyobb = negyedikloves;
+            }
+            return legnagyobb;
+        }
+        static void Main(string[] args)
+        {
+            List<cellovo> lovesek = new List<cellovo>();
+            foreach (var sor in File.ReadLines("lovesek.csv"))
+            {
+                lovesek.Add(new cellovo(sor));
+            }
+
+            foreach (var cellovo in lovesek)
+            {
+                Console.WriteLine(cellovo.nev + " " + legnagyobb(cellovo.elsoloves, cellovo.masodikloves, cellovo.harmadikloves, cellovo.negyedikloves));
+            }
+
+            cellovo legtobbPont = lovesek[0];
+            foreach (var cellovo in lovesek)
+            {
+                if (legnagyobb(legtobbPont.elsoloves, legtobbPont.masodikloves, legtobbPont.harmadikloves, legtobbPont.negyedikloves) < legnagyobb(cellovo.elsoloves, cellovo.masodikloves, cellovo.harmadikloves, cellovo.negyedikloves))
+                {
+                    legtobbPont = cellovo;
+                }
+            }
+            Console.WriteLine("A legnagyobb találatot lövő eredményei:");
+            Console.WriteLine(legtobbPont.nev + " " + legtobbPont.elsoloves + " " + legtobbPont.masodikloves + " " + legtobbPont.harmadikloves + " " + legtobbPont.negyedikloves);
+
+            cellovo leggyengebb = lovesek[0];
+            foreach (var cellovo in lovesek)
+            {
+                if ((leggyengebb.elsoloves + leggyengebb.masodikloves + leggyengebb.harmadikloves + leggyengebb.negyedikloves)/4 > (cellovo.elsoloves + cellovo.masodikloves + cellovo.harmadikloves + cellovo.negyedikloves) / 4)
+                {
+                    leggyengebb = cellovo;
+                }
+            }
+            Console.WriteLine("A leggyengebbatlagu találatot lövő eredményei:");
+            Console.WriteLine(leggyengebb.nev + " " + (leggyengebb.elsoloves + leggyengebb.masodikloves + leggyengebb.harmadikloves + leggyengebb.negyedikloves) / 4);
+        }
+    }
+}
---
+++ konzol/celloveszetCLI/celloveszetCLI/cellovo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace celloveszetCLI
+{
+    internal class cellovo
+    {
+        public string nev {  get; private set; }
+        public int elsoloves { get; private set; }
+        public int masodikloves  { get; private set; }
+        public int harmadikloves { get; private set; }
+        public int negyedikloves { get; private set; }
+        public cellovo(string sor)
+        {
+            string[] adat = sor.Split(";");
+            nev = adat[0];
+            elsoloves = int.Parse(adat[1]);
+            masodikloves = int.Parse(adat[2]);
+            harmadikloves = int.Parse(adat[3]);
+            negyedikloves = int.Parse(adat[4]);
+        }
+    }
+}
