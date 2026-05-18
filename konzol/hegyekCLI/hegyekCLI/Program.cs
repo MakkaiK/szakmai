5dd8de6c25851aa8fedcc825254e2781cc1d3f98
MakkaiK <90418493+MakkaiK@users.noreply.github.com>
2026-05-18 21:18:27 +0200

    Add files via upload

---
+++ konzol/hegyekCLI/hegyekCLI/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace hegyekCLI
+{
+    public class Program
+    {
+        public static bool tartalmaz(string keresett, string nev, string hegyseg)
+        {
+            if(nev.Contains(keresett) || hegyseg.Contains(keresett))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<hegycsucs> hegyek = new List<hegycsucs>();
+            var sorok = File.ReadAllLines("hegyek.csv").Skip(1);
+            foreach (var sor in sorok)
+            {
+                hegyek.Add(new hegycsucs(sor));
+            }
+
+            foreach (var hegy in hegyek)
+            {
+                if (hegy.magassag > 950)
+                {
+                    Console.WriteLine($"{hegy.nev} {hegy.hegyseg} {hegy.magassag}");
+                }
+            }
+
+            Console.WriteLine("Kérem a keresett szüt:");
+            string keresett = Console.ReadLine();
+            Console.WriteLine("........");
+            foreach (var hegy in hegyek)
+            {
+                if (tartalmaz(keresett, hegy.nev, hegy.hegyseg))
+                {
+                    Console.WriteLine(hegy.nev);
+                }
+            }
+        }
+    }
+}
---
+++ konzol/hegyekCLI/hegyekCLI/hegycsucs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hegyekCLI
+{
+    public class hegycsucs
+    {
+        public string nev {  get; private set; }
+        public string hegyseg { get; private set; }
+        public int magassag { get; private set; }
+
+        public hegycsucs(string sor)
+        {
+            string[] adatok = sor.Split(";");
+            nev = adatok[0];
+            hegyseg = adatok[1];
+            magassag = int.Parse(adatok[2]);
+        }
+    }
+}
